a7227c609cb483d6df6a3b45fd5119845172b8e9
Asakuri <41738319+Noki-Asakuri@users.noreply.github.com>
2023-12-04 09:40:49 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,16 @@
+function skillMembers() {
+	var members = [
+		{ name: "Tom", skills: ["JavaScript", "CSS", "HTML"] },
+		{ name: "Bob", skills: ["JavaScript", "Node.js", "Ruby"] },
+		{ name: "Alice", skills: ["Python", "MongoDB"] },
+	];
+	var result = [];
+	members.forEach(function (member) {
+		member.skills.forEach(function (skill) {
+			if (result.indexOf(skill) === -1) {
+				result.push(skill);
+			}
+		});
+	});
+	return result;
+}
